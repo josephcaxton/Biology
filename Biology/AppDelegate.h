19fe1817a8af8b0566733f305a03af66f802ba9d
Joseph caxton-Idowu <Joseph@Joseph-caxton-Idowus-iMac.local>
2013-04-16 10:06:19 +0100

    Initial Commit

---
+++ Biology/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Biology
+//
+//  Created by Joseph caxton-Idowu on 16/04/2013.
+//  Copyright (c) 2013 LearnersCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ Biology/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  Biology
+//
+//  Created by Joseph caxton-Idowu on 16/04/2013.
+//  Copyright (c) 2013 LearnersCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
